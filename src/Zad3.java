4a4b2636642d5aa8b70d7e46ec2321a193735f50
ZaharovArt <zaharov-art@mail.ru>
2023-06-14 21:19:03 +0300

    1

--- src/Zad3.java
+++ src/Zad3.java
@@ -11,6 +11,38 @@ public class Zad3 {
         Scanner scanner = new Scanner(System.in);
         chain = scanner.nextInt();
 
+        switch (chain){
+            case 1:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 2:
+                System.out.println("Гав-Гав");
+                break;
+            case 3:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 4:
+                System.out.println("Хрю-Хрю");
+                break;
+            case 5:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 6:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 7:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 8:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 9:
+                System.out.println("Мяу-Мяу");
+                break;
+            case 10:
+                System.out.println("Мяу-Мяу");
+                break;
+        }
 
     }
 }
